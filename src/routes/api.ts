b58992d7cf2516be78d0941df1ad2b439967ce6b
Amós Silas <silastj@hotmail.com>
2022-03-20 00:33:17 -0300

    remove /

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,14 +3,14 @@ import * as ApiController from '../controllers/apiController'
 
 const router = Router();
 
-router.get('/ping', ApiController.ping);
-router.get('/random', ApiController.random);
-router.get('/nome/:nome', ApiController.nome);
-router.post('/carros', ApiController.createCars);
-router.get('/carros', ApiController.listCars);
-router.get('/carros/aleatoria', ApiController.ramdomCars);
-router.get('/carros/:id', ApiController.getCars);
-router.put('/carros/:id', ApiController.updateCars);
-router.delete('/carros/:id', ApiController.deleteCars);
+router.get('ping', ApiController.ping);
+router.get('random', ApiController.random);
+router.get('nome/:nome', ApiController.nome);
+router.post('carros', ApiController.createCars);
+router.get('carros', ApiController.listCars);
+router.get('carros/aleatoria', ApiController.ramdomCars);
+router.get('carros/:id', ApiController.getCars);
+router.put('carros/:id', ApiController.updateCars);
+router.delete('carros/:id', ApiController.deleteCars);
 
 export default router;
