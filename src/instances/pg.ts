ea4587c05080291da97f8d00e281943f98edd5d0
Amós Silas <silastj@hotmail.com>
2022-03-20 23:49:58 -0300

    add5

--- src/instances/pg.ts
+++ src/instances/pg.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
 export const sequelize = new Sequelize(
     process.env.MYSQL_DB as string,
